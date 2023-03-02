b3ea84731a7575671f8ce7485822f5da220681f4
Aleksandr-Viktorovich <teanvkz@gmail.com>
2023-03-03 04:02:27 +0700

    правки

--- game.js
+++ game.js
@@ -11,12 +11,14 @@ const getRandom = (one, two) => {
 
   const langObj = {
     'RU': {
+      figures: ['камень', 'ножницы', 'бумага'],
       startGame: 'камень, ножницы, бумага?',
       validValue: 'Не корректное значение!',
       exit: 'Ты уверен?',
       endGame: 'СЧЕТ:',
     },
     'ENG': {
+      figures: ['rock', 'scissors', 'paper'],
       startGame: 'rock, scissors, paper?',
       validValue: 'Please enter a valid value',
       exit: 'You are sure?',
@@ -32,7 +34,6 @@ const game = (language) => {
       return `\nSkyNet-${result.SkyNet} \nКожаный-${result.user}`;
     },
   };
-
   const lang = language === 'EN' || language === 'ENG' ? FIGURES_ENG : FIGURES_rus;
 
   return function start () {
@@ -46,34 +47,17 @@ const game = (language) => {
       }
       return notification;
     };
-
     const langSelect = notification();
 
-    const skyNetPlay = () => {
-      let skyResult = '';
-      let skyItem = getRandom(0, 3)
-      if (skyItem === 0) {
-        skyResult = 'камень';
-        return skyResult;
-      }
-      if (skyItem === 1) {
-        skyResult = 'ножницы';
-        return skyResult;
-      } else {
-        skyResult = 'бумага';
-        return skyResult;
-      }
-    };
      const userPlay = () => {
-       let userResult = '';
+       let userResult;
        let userItem = prompt(`${langSelect.startGame}`, '');
         if (userItem) {
-         if (userItem === FIGURES_rus[0] || userItem === FIGURES_rus[1] || userItem === FIGURES_rus[2] ||
-           userItem === FIGURES_ENG[0] || userItem === FIGURES_ENG[1] || userItem === FIGURES_ENG[2]) {
-           userResult = userItem
+         if (userItem === langSelect.figures[0] || userItem === langSelect.figures[1] || userItem === langSelect.figures[2]) {
+           userResult = userItem;
          } else {
            alert(`${langSelect.validValue}`);
-           return userResult;
+           return userPlay();
          }
        }
         if (userItem === null) {
@@ -82,31 +66,24 @@ const game = (language) => {
             return alert(`${langSelect.endGame} ${result.gameResult}`);
           }
         }
-        return userResult
+        return userResult;
    };
-    let inputUser = userPlay()
-    let inputSky = skyNetPlay()
+
+    let inputUser = userPlay();
+    let userRes = langSelect.figures.indexOf(inputUser);
+    let inputSky = getRandom(0, 3);
      const win = () => {
 
-       let gameRes = inputUser + inputSky
-       console.log(gameRes)
-       switch (gameRes) {
-         case 'каменькамень' || 'rockrock':
-         case 'ножницыножницы' || 'scissorsscissors':
-         case 'бумагабумага' || 'paperpaper':
-           return  result;
-
-         case 'каменьножницы' || 'rockscissors':
-         case 'бумагакамень' || 'paperrock':
-         case 'ножницыбумага' || 'scissorspaper':
-           return result.user++;
-
-         case 'каменьбумага' || 'prockaper':
-         case 'бумаганожницы' || 'paperscissors':
-         case 'ножницыкамень' || 'scissorsrock':
-           return result.SkyNet++;
+       if (userRes === inputSky) {
+         return result.user;
        }
-     }
+       if (userRes === 0 && inputSky === 1 || userRes === 1 && inputSky === 2
+          || userRes === 2 && inputSky === 0) {
+         return  result.user++;
+       } else {
+         return result.SkyNet++;
+       }
+     };
       win();
     if (inputUser) {
       return start();
